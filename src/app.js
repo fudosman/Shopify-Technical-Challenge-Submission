e66877674eadf24c35d3d4fff601c3b19066afad
afudoh kachi <fudosman@gmail.com>
2022-05-17 17:12:34 +0100

    proper

--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ const app = express();
 const globalErrorHandler = require("./errors/app_error_handler");
 require("./middlewares/pre-route.middleware")(app);
 
-// home route for documentation
-app.use("/", (req, res) => {
-  res.status(StatusCodes.ACCEPTED).send(`Visit this link for the Shopify API <a href="https://documenter.getpostman.com/view/19482223/Uyxkk5ZX"> Documentation </a>`);
-});
+
 // API Version 1
 app.use("/api", require("./routes/versions"));
 
 // PING
 app.get("/ping", (req, res) => res.status(StatusCodes.OK).send("the server is running!"));
+
+// home route for documentation
+app.use("/", (req, res, next) => {
+  res.status(StatusCodes.ACCEPTED).send(`Visit this link for the Shopify API <a href="https://documenter.getpostman.com/view/19482223/Uyxkk5ZX"> Documentation </a>`);
+});
 
 // Not Found route
 app.all("*", (req, res) => {
@@ -21,6 +23,7 @@ app.all("*", (req, res) => {
     message: "Can't find " + req.originalUrl + " on this server",
   });
 });
+
 
 // Error middlewares
 app.use(globalErrorHandler);
